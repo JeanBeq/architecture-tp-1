4d3a4de491ab0a08652fe6b7a31d08ddcde58284
Jean B <jean.bequilleux@gmail.com>
2024-10-08 16:00:36 +0200

    Add axios dependency and update package-lock.json

--- src/interfaces/http/userController.js
+++ src/interfaces/http/userController.js
@@ -6,6 +6,8 @@ const UpdateUserCommand = require('../../application/commands/updateUserCommand'
 const DeleteUserCommand = require('../../application/commands/deleteUserCommand');
 const GetAllUsersQuery = require('../../application/queries/getAllUsersQuery');
 
+const axios = require('axios');
+
 const createUserCommand = new CreateUserCommand(UserModel, ProfileModel);
 const updateUserCommand = new UpdateUserCommand(UserModel);
 const deleteUserCommand = new DeleteUserCommand(UserModel);
@@ -21,11 +23,21 @@ exports.getAllUsers = async (req, res) => {
 };
 
 exports.createUser = async (req, res) => {
+  const transaction = await UserModel.sequelize.transaction();
   try {
     const { email, lastName, firstName, phoneNumber, profileId } = req.body;
     const newUser = await createUserCommand.execute({ email, lastName, firstName, phoneNumber, profileId });
+
+    await axios.post('http://localhost:3001/accounts/new', {
+      userId: newUser.id,
+      accountNumber: `ACC-${newUser.id}`,
+      balance: 0
+    });
+
+    await transaction.commit();
     res.status(201).json(newUser);
   } catch (error) {
+    await transaction.rollback();
     res.status(500).json({ error: error.message });
   }
 };
@@ -45,11 +57,17 @@ exports.updateUser = async (req, res) => {
 };
 
 exports.deleteUser = async (req, res) => {
+  const transaction = await UserModel.sequelize.transaction();
   try {
     const { id } = req.params;
     await deleteUserCommand.execute(id);
+
+    await axios.delete(`http://localhost:3001/accounts/${id}`);
+
+    await transaction.commit();
     res.status(204).send();
   } catch (error) {
+    await transaction.rollback();
     res.status(500).json({ error: error.message });
   }
 };
